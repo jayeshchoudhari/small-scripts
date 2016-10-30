421a5c0c3202cdac5d34bbb25106ae3531d0d3e4
Jayesh Choudhari <choudhari.jayesh@iitgn.ac.in>
2016-10-30 11:40:26 +0530

    Update change-filenames.sh

--- change-filenames.sh
+++ change-filenames.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
 _dfiles="*.txt"
-echo $_dfiles
 
-count=10
+#have a look at the files that you would be renaming....
+#echo $_dfiles 
+
+count=0
 underscore="_"
 for file in $_dfiles
 do
 	lastpart=$(echo $file| cut -d'_' -f 2)
+	#the filenames is renamed to "count_original_filename"
 	newname="$count$underscore$lastpart"
 	mv $file $newname
 	count=$(($count+1))
